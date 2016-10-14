255065bd4a6f65a1817b4e42f44f6a8bf5d6a3d5
dingweili123 <dreamit123@sina.com>
2016-10-14 15:04:14 +0800

    正式提交项目

--- app/src/main/java/com/example/abc/myprojectnum0/MainActivity.java
+++ app/src/main/java/com/example/abc/myprojectnum0/MainActivity.java
@@ -1,18 +1,58 @@
 package com.example.abc.myprojectnum0;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_pm).setOnClickListener(this);
+        findViewById(R.id.btn_sh).setOnClickListener(this);
+        findViewById(R.id.btn_bib).setOnClickListener(this);
+        findViewById(R.id.btn_myam).setOnClickListener(this);
+        findViewById(R.id.btn_gu).setOnClickListener(this);
+        findViewById(R.id.btn_cap).setOnClickListener(this);
     }
 
-    private void init(){
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_pm:
+                showToast("POPULAR MOVIES");
+                break;
+
+            case R.id.btn_sh:
+                showToast("STOCK HAWK");
+                break;
+
+            case R.id.btn_bib:
+                showToast("BUILD IT BIGGER");
+                break;
+
+            case R.id.btn_myam:
+                showToast("MAKE YOUR APP MATERIAL");
+                break;
+
+            case R.id.btn_gu:
+                showToast("GO UBIQUITOUS");
+                break;
+
+            case R.id.btn_cap:
+                showToast("CAPSTONE");
+                break;
+        }
+    }
+
+    private void showToast(String msg) {
+        Toast.makeText(this, "This button will launch my " + msg + "  app!", Toast.LENGTH_SHORT).show();
 
     }
+
 
 }
